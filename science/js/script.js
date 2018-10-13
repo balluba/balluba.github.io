8787b858ab0829dbed7cbc2e6bc05b234ac5e119
balluba <43842221+balluba@users.noreply.github.com>
2018-10-14 00:18:38 +0530

    re

---
+++ science/js/script.js
@@ -0,0 +1,21 @@
+$(function() {
+  $(".mobilebar").click(function(){
+    $(".nav-container nav").toggle(300);
+    $(".mobilebar i").toggleClass("fa-bars fa-remove");
+  });
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
