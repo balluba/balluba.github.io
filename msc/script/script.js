dfddceb3fa173fdf8038fe45d6e9b99438fafc10
balluba <43842221+balluba@users.noreply.github.com>
2021-08-03 15:12:12 +0530

    up

--- msc/script/script.js
+++ msc/script/script.js
@@ -12,6 +12,8 @@ $(function() {
     $(".toggleIndia").css('display','none');
     $(".manipur").addClass("selected");
     $(".india").removeClass("selected");
+    $(".in").css('display','none');
+    $(".mn").css('display','inline-block');
   });
 });
 
@@ -21,5 +23,35 @@ $(function() {
     $(".toggleManipur").css('display','none');
     $(".india").addClass("selected");
     $(".manipur").removeClass("selected");
+    $(".in").css('display','inline-block');
+    $(".mn").css('display','none');
+  });
+});
+
+$(document).ready(function() {
+  $(".set > span").on("click", function() {
+    if ($(this).hasClass("act")) {
+      $(this).removeClass("act");
+      $(this)
+        .siblings(".content")
+        .slideUp(200);
+      $(".set > span i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+    } else {
+      $(".set > span i")
+        .removeClass("fa-minus")
+        .addClass("fa-plus");
+      $(this)
+        .find("i")
+        .removeClass("fa-plus")
+        .addClass("fa-minus");
+      $(".set > span").removeClass("act");
+      $(this).addClass("act");
+      $(".content").slideUp(200);
+      $(this)
+        .siblings(".content")
+        .slideDown(200);
+    }
   });
 });
